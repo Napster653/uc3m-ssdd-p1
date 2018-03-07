0e3a07331ca5035d2072e7df43ca79b60be28b9e
Napster653 <100330656@alumnos.uc3m.es>
2018-03-07 20:37:06 +0100

    nombre en init()

--- claves.c
+++ claves.c
@@ -4,6 +4,8 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int init ()
 {
@@ -16,21 +18,21 @@ int init ()
 	attr.mq_msgsize = sizeof(int);
 
 	char name[128];
-	sprintf(name, "/")
-	q_client = mq_open ("/QueueClient-%d", O_CREAT|O_RDONLY, 0700, &q_attr); // Nombre TBD
+	sprintf(name, "/QueueClient-%d", getpid());
+	q_client = mq_open ((const char*) name, O_CREAT|O_RDONLY, 0700, &q_attr);
 	q_server = mq_open ("/Queue656", O_WRONLY);
 
 	req.op = 0;
 	// Value1
 	// Value2
-	strcpy (req.q_name, ""); // Nombre TBD
+	strcpy (req.q_name, (const char*) name);
 
 	mq_send (q_server, (const char*) &req, sizeof(struct request), 0);
 	mq_receive (q_client, (char*) &res, sizeof(int), 0);
 
 	mq_close (q_server);
 	mq_close (q_client);
-	mq_unlink (""); // Nombre TBD
+	mq_unlink ((const char*) name);
 }
 
 int set_value ()
