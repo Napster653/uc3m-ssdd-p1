f668d2adbc4e23382e6b4f691c45e927bced0a29
Napster653 <100330656@alumnos.uc3m.es>
2018-03-08 22:34:21 +0100

    .

--- lista.h
+++ lista.h
@@ -2,30 +2,30 @@
 #define TRUE 1
 #define FALSE 0
 
-//DEFINICIÓN DEL NODO DE LA LISTA
-struct node {
-    //DATOS
-    int key;
-    char * value1;
-    float value2;
-    //Puntero al siguiente elemento de la lista (Singly-linked)
-    struct node * next;
+// DEFINICIÓN DEL NODO DE LA LISTA
+struct node
+{
+	// DATOS
+	int key;
+	char * value1;
+	float value2;
+	// Puntero al siguiente elemento de la lista (Singly-linked)
+	struct node * next;
 };
 typedef struct node *Node;
 
-//ATRIBUTOS DE NUESTRA SINGLY-LINKED LIST
-Node head;//Primer nodo
-int size;//Número de elementos
-int active;//active=TRUE --> lista iniciada/creada; active=FALSE --> lista no iniciada/creada
+// ATRIBUTOS DE NUESTRA SINGLY-LINKED LIST
+Node head; // Primer nodo
+int size; // Número de elementos
+int active; // active=TRUE --> lista iniciada/creada; active=FALSE --> lista no iniciada/creada
 
-//FUNCIONES DE LA LISTA
-int getState();
-int createList();
-struct node createNode(int k, char * v1, float v2);
-struct node getNode(int k);
-int addNode(int k, char * v1, float v2);
-int removeNode(int k);
-int edit(int k, char * v1, float v2);
-int removeList();
-int getSize();
-
+// FUNCIONES DE LA LISTA
+int getState ();
+int createList ();
+Node createNode (int k, char * v1, float v2);
+Node getNode (int k);
+int addNode (int k, char * v1, float v2);
+int removeNode (int k);
+int edit (int k, char * v1, float v2);
+int removeList ();
+int getSize ();
